44b6473deaeffb89659232bca57ca4a11e141950
Marina Vel <marinavelitskaia@gmail.com>
2021-07-26 12:53:39 -0400

    test case all done

--- src/ResettingPractice.java
+++ src/ResettingPractice.java
@@ -1,7 +1,11 @@
 public class ResettingPractice {
     //1-st change
     //2-nd change
-//3-rd change
+    //3-rd change
+    //4-th change
+    //5-th change
+    //mixed soft reset remove the commit history but keep the local changes
+    //did part 4 part5 and. I am done in one commit
 
 
 }
