11e1a108fb04031f9885dad199a3690d5a52633e
Marina Vel <marinavelitskaia@gmail.com>
2021-07-29 14:33:53 -0400

    commit 3

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,6 +1,14 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
     //list practice 1
     //list practice 2
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        //adding 1 item
+    }
 }
