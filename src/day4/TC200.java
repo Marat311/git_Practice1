66f932657e0ee5dac1b7b258add5879986404346
Marina Vel <marinavelitskaia@gmail.com>
2021-08-05 14:18:14 -0400

    commit3  from us200

--- src/day4/TC200.java
+++ src/day4/TC200.java
@@ -2,4 +2,17 @@ package day4;
 
 public class TC200 {
     //finish tc 200
+    public static void main(String[] args) {
+        Boolean b = new Boolean("true");
+        Boolean b1 = new Boolean("tRue");
+        System.out.println(b.equals(b1));
+
+        Boolean r1 = Boolean.parseBoolean("TrUe");
+        Boolean r2 = Boolean.parseBoolean("true");
+        System.out.println("r1 = " + r1); //true
+        System.out.println("r2 = " + r2); //true
+        //new commit
+
+
+    }
 }
